cb9ea3c1f7c7c1a0ccad572aa8aef073e857ba58
GuilhermeDeOliveiraAmorim <guilherme.o.a.ufal@gmail.com>
2024-02-08 21:22:32 -0300

    feat: adds get chooser by email

--- internal/repository_interface/repository_interface_chooser.go
+++ internal/repository_interface/repository_interface_chooser.go
@@ -6,6 +6,7 @@ type ChooserRepositoryInterface interface {
 	Create(chooser *entity.Chooser) error
 	Update(chooser *entity.Chooser) error
 	GetByID(chooserID string) (entity.Chooser, error)
+	GetByEmail(chooserEmail string) (entity.Chooser, error)
 	GetAll() ([]entity.Chooser, error)
 	Deactivate(chooserID string) error
 	GetLists(chooserID string) error
