9a56cf8e640ed02d486f7995d131ba2e8b401d02
GuilhermeDeOliveiraAmorim <guilherme.o.a.ufal@gmail.com>
2024-02-16 10:50:11 -0300

    feat: adds boolean to get list and votation by id

--- internal/repository_interface/repository_interface_list.go
+++ internal/repository_interface/repository_interface_list.go
@@ -5,7 +5,7 @@ import "youchoose/internal/entity"
 type ListRepositoryInterface interface {
 	Create(list *entity.List) error
 	Update(list *entity.List) error
-	GetByID(listID string) (entity.List, error)
+	GetByID(listID string) (bool, entity.List, error)
 	GetAll() ([]entity.List, error)
 	Deactivate(listID string) error
 	GetAllMoviesByListID(listID string) ([]entity.Movie, error)
--- internal/repository_interface/repository_interface_votation.go
+++ internal/repository_interface/repository_interface_votation.go
@@ -5,7 +5,7 @@ import "youchoose/internal/entity"
 type VotationRepositoryInterface interface {
 	Create(votation *entity.Votation) error
 	Update(votation *entity.Votation) error
-	GetByID(votationID string) (entity.Votation, error)
+	GetByID(votationID string) (bool, entity.Votation, error)
 	GetAll() ([]entity.Votation, error)
 	GetActivesByListID(listID string) ([]entity.Votation, error)
 	Deactivate(votationID string) error
