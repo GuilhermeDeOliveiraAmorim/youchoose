88358010fab6456aede68c8cc4148eb8eadbe893
GuilhermeDeOliveiraAmorim <guilherme.o.a.ufal@gmail.com>
2024-04-13 18:26:09 -0300

    feat: adds shared error messages and actor error messages detail

--- internal/util/util_errors_messages.go
+++ internal/util/util_errors_messages.go
@@ -7,3 +7,18 @@ const (
 	TypeNotFound            = "Not Found"
 	TypeConflict            = "Conflict"
 )
+
+const (
+	SharedErrorTitleInvalidName        = "Nome inválido"
+	SharedErrorTitleInvalidBirthDate   = "Data de nascimento inválida"
+	SharedErrorTitleInvalidNationality = "Nacionalidade inválida"
+	SharedErrorTitleInvalidImageID     = "ID de imagem inválido"
+)
+
+const (
+	ActorErrorDetailEmptyName          = "O nome do(a) ator(atriz) não pode estar vazio"
+	ActorErrorDetailMaxLengthName      = "O nome do(a) ator(atriz) não pode ter mais do que 100 caracteres"
+	ActorErrorDetailNotNullBirthDate   = "A data de nascimento do(a) ator(atriz) não pode ser nula"
+	ActorErrorDetailNotNullNationality = "A nacionalidade do(a) ator(atriz) não pode ser nula"
+	ActorErrorDetailEmptyImageID       = "O ID de imagem do(a) ator(atriz) não pode estar vazio"
+)
