6bf52f97ceb1fe821f5a43d2118ef7cf40b21fde
GuilhermeDeOliveiraAmorim <guilherme.o.a.ufal@gmail.com>
2024-04-13 17:56:53 -0300

    feat: adds error type library to problem details

---
+++ internal/util/util_errors_messages.go
@@ -0,0 +1,9 @@
+package util
+
+const (
+	TypeValidationError     = "Validation Error"
+	TypeInternalServerError = "Internal Server Error"
+	TypeBadRequest          = "Bad Request"
+	TypeNotFound            = "Not Found"
+	TypeConflict            = "Conflict"
+)
