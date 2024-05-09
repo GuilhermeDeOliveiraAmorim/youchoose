8455ce90a44c0042e2c33e8c340f4080227fcd15
GuilhermeDeOliveiraAmorim <guilherme.o.a.ufal@gmail.com>
2024-05-08 23:35:48 -0300

    feat: adds gorm image model and image and chooser relationships

--- internal/infra/repository/models.go
+++ internal/infra/repository/models.go
@@ -19,5 +19,18 @@ type Choosers struct {
 	Day           int       `gorm:"not null"`
 	Month         int       `gorm:"not null"`
 	Year          int       `gorm:"not null"`
-	ImageID       string    `gorm:"not null"`
+	ImageID       string    `gorm:"not null;foreignKey:ID"`
+	Image         Images    `gorm:"foreignKey:ImageID"`
+}
+
+type Images struct {
+	ID            string     `gorm:"primaryKey;not null"`
+	Active        bool       `gorm:"not null"`
+	CreatedAt     time.Time  `gorm:"not null"`
+	UpdatedAt     time.Time  `gorm:"not null"`
+	DeactivatedAt time.Time  `gorm:"not null"`
+	Name          string     `gorm:"not null"`
+	Type          string     `gorm:"not null"`
+	Size          int64      `gorm:"not null"`
+	Choosers      []Choosers `gorm:"foreignKey:ImageID"`
 }
