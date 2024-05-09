0dddd3c55e118cc05aed581d743c653f4239c302
GuilhermeDeOliveiraAmorim <guilherme.o.a.ufal@gmail.com>
2024-05-08 23:33:59 -0300

    feat: adds UpdateChooser to chooser factory

--- internal/infra/factory/chooser_factory.go
+++ internal/infra/factory/chooser_factory.go
@@ -11,18 +11,22 @@ type ChooserFactory struct {
 	CreateChooser   *usecase.CreateChooserUseCase
 	FindChooserByID *usecase.GetChooserUseCase
 	GetChoosers     *usecase.GetChoosersUseCase
+	UpdateChooser   *usecase.UpdateChooserUseCase
 }
 
 func NewChooserFactory(db *gorm.DB) *ChooserFactory {
 	chooserRepository := repository.NewChooserRepository(db)
+	imageRepository := repository.NewImageRepository(db)
 
-	createChooser := usecase.NewCreateChooserUseCase(chooserRepository)
+	createChooser := usecase.NewCreateChooserUseCase(chooserRepository, imageRepository)
 	findChooserByID := usecase.NewGetChooserUseCase(chooserRepository)
 	getChoosers := usecase.NewGetChoosersUseCase(chooserRepository)
+	updateChooser := usecase.NewUpdateChooserUseCase(chooserRepository, imageRepository)
 
 	return &ChooserFactory{
 		CreateChooser:   createChooser,
 		FindChooserByID: findChooserByID,
 		GetChoosers:     getChoosers,
+		UpdateChooser:   updateChooser,
 	}
 }
