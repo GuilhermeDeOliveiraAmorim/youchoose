52dfae089eb9cb9ab27224500e259b3cec6290c9
GuilhermeDeOliveiraAmorim <guilherme.o.a.ufal@gmail.com>
2024-05-15 14:48:02 -0300

    feat: adds DoTheseWritersExist implementation

--- internal/infra/repository/writer_repository_gorm.go
+++ internal/infra/repository/writer_repository_gorm.go
@@ -1,56 +1,86 @@
 package gorm
 
 import (
-	"gorm.io/gorm"
 	"youchoose/internal/entity"
+	valueobject "youchoose/internal/value_object"
+
+	"gorm.io/gorm"
 )
 
 type WriterRepository struct {
 	gorm *gorm.DB
 }
 
-// Create implements repositoryinterface.WriterRepositoryInterface.
-func (w *WriterRepository) Create(writer *entity.Writer) error {
-	panic("unimplemented")
-}
-
-// CreateMany implements repositoryinterface.WriterRepositoryInterface.
-func (w *WriterRepository) CreateMany(writers *[]entity.Writer) error {
-	panic("unimplemented")
-}
-
-// Deactivate implements repositoryinterface.WriterRepositoryInterface.
-func (w *WriterRepository) Deactivate(writer *entity.Writer) error {
-	panic("unimplemented")
-}
-
-// DoTheseWritersAreIncludedInTheMovie implements repositoryinterface.WriterRepositoryInterface.
-func (w *WriterRepository) DoTheseWritersAreIncludedInTheMovie(movieID string, writersIDs []string) (bool, []entity.Writer, error) {
-	panic("unimplemented")
-}
-
-// DoTheseWritersExist implements repositoryinterface.WriterRepositoryInterface.
-func (w *WriterRepository) DoTheseWritersExist(writerIDs []string) (bool, []entity.Writer, error) {
-	panic("unimplemented")
-}
-
-// GetAll implements repositoryinterface.WriterRepositoryInterface.
-func (w *WriterRepository) GetAll() ([]entity.Writer, error) {
-	panic("unimplemented")
-}
-
-// GetByID implements repositoryinterface.WriterRepositoryInterface.
-func (w *WriterRepository) GetByID(writerID string) (entity.Writer, error) {
-	panic("unimplemented")
-}
-
-// Update implements repositoryinterface.WriterRepositoryInterface.
-func (w *WriterRepository) Update(writer *entity.Writer) error {
-	panic("unimplemented")
-}
-
 func NewWriterRepository(gorm *gorm.DB) *WriterRepository {
 	return &WriterRepository{
 		gorm: gorm,
 	}
+}
+
+func (w *WriterRepository) Create(writer *entity.Writer) error {
+	panic("unimplemented")
+}
+
+func (w *WriterRepository) CreateMany(writers *[]entity.Writer) error {
+	panic("unimplemented")
+}
+
+func (w *WriterRepository) Deactivate(writer *entity.Writer) error {
+	panic("unimplemented")
+}
+
+func (w *WriterRepository) DoTheseWritersAreIncludedInTheMovie(movieID string, writersIDs []string) (bool, []entity.Writer, error) {
+	panic("unimplemented")
+}
+
+func (w *WriterRepository) DoTheseWritersExist(writerIDs []string) (bool, []entity.Writer, error) {
+	var writersModel []Writers
+	result := w.gorm.Where("id IN ?", writerIDs).Find(&writersModel)
+
+	if result.Error != nil {
+		return false, nil, result.Error
+	}
+
+	var writers []entity.Writer
+
+	if result.RowsAffected != int64(len(writerIDs)) {
+		return false, writers, nil
+	}
+
+	for _, writerModel := range writersModel {
+		writers = append(writers, entity.Writer{
+			SharedEntity: entity.SharedEntity{
+				ID:            writerModel.ID,
+				Active:        writerModel.Active,
+				CreatedAt:     writerModel.CreatedAt,
+				UpdatedAt:     writerModel.UpdatedAt,
+				DeactivatedAt: writerModel.DeactivatedAt,
+			},
+			Name:    writerModel.Name,
+			ImageID: writerModel.ImageID,
+			BirthDate: &valueobject.BirthDate{
+				Day:   writerModel.Day,
+				Month: writerModel.Month,
+				Year:  writerModel.Year,
+			},
+			Nationality: &valueobject.Nationality{
+				CountryName: writerModel.CountryName,
+				Flag:        writerModel.Flag,
+			},
+		})
+	}
+
+	return true, writers, nil
+}
+
+func (w *WriterRepository) GetAll() ([]entity.Writer, error) {
+	panic("unimplemented")
+}
+
+func (w *WriterRepository) GetByID(writerID string) (entity.Writer, error) {
+	panic("unimplemented")
+}
+
+func (w *WriterRepository) Update(writer *entity.Writer) error {
+	panic("unimplemented")
 }
